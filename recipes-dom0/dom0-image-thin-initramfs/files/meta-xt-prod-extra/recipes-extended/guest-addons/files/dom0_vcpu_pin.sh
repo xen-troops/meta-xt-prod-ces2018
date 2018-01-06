9a3ba1b1cc2acccdcb31cb1dc6ffc5eeefbeabe4
Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
2018-01-06 14:09:52 +0200

    Fixup files where we pin VCPUs for certain domains
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
    Suggested-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    Suggested-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Reviewed-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- recipes-dom0/dom0-image-thin-initramfs/files/meta-xt-prod-extra/recipes-extended/guest-addons/files/dom0_vcpu_pin.sh
+++ recipes-dom0/dom0-image-thin-initramfs/files/meta-xt-prod-extra/recipes-extended/guest-addons/files/dom0_vcpu_pin.sh
@@ -7,6 +7,6 @@
 # Default-Stop:
 ### END INIT INFO
 
-# Force all VCPUs of Domain-0 to only run on PCPUs from 4 to 7 (A53 cores)
+# Force all VCPUs of Domain-0 to only run on DOM0_ALLOWED_PCPUS PCPUs (A53 cores)
 echo "Pinning Domain-0 VCPUs"
-xl vcpu-pin Domain-0 all 4-7
+xl vcpu-pin Domain-0 all DOM0_ALLOWED_PCPUS
